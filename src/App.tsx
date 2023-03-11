cecae1c2eb819594f7dd9bf5180ae69bc6b5477c
devstefancho <skku7714@gmail.com>
2023-03-11 22:51:38 +0900

    💄 add custom select box with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import ArrowDown from "./assets/arrow-down.icon";
 import CopyIcon from "./assets/copy.icon";
 import PipetteIcon from "./assets/pipette.icon";
 
@@ -6,11 +7,23 @@ type EyeDropperResponse = {
   sRGBHex: string;
 };
 
+enum Mode {
+  Hex = "Hex",
+  Hsl = "Hsl",
+}
+
 function App() {
   const [pickedColor, setPickedColor] = useState("");
+  const [mode, setMode] = useState<Mode>(Mode.Hex);
+  const [isOpenSelectBox, setIsOpenSelectBox] = useState(false);
   const clickPicker = async () => {
     const result: EyeDropperResponse = await new window.EyeDropper().open();
     setPickedColor(result.sRGBHex);
+  };
+
+  const clickOption = (mode: Mode) => {
+    setMode(mode);
+    setIsOpenSelectBox(false);
   };
 
   return (
@@ -20,6 +33,7 @@ function App() {
         <div className="title">Color dropper</div>
       </header>
       <main className="color-picker-body">
+        {/* 스포이드 라인 */}
         <div className="color-picker">
           <button onClick={clickPicker}>
             <PipetteIcon />
@@ -29,13 +43,40 @@ function App() {
             style={{ backgroundColor: pickedColor }}
           ></span>
         </div>
-        <div className="color-code-container">
-          {/* <label htmlFor="color-code--selector"></label> */}
-          <select id="color-code--selector" name="color-code--selector">
-            <option value="hex">Hex</option>
-            <option value="hsl">HSL</option>
-          </select>
-          <div className="color-code">
+        {/* Select Box 라인 */}
+        <div className="relative color-code-container cursor-pointer select-none">
+          <div
+            className={`select-rect rounded-[4px] hover:border-black ${
+              isOpenSelectBox
+                ? "rounded-bl-[0px] rounded-br-[0px] hover:border-gray-bg"
+                : ""
+            }`}
+            onClick={() => setIsOpenSelectBox((prev) => !prev)}
+          >
+            <div>{mode}</div>
+            <div>
+              <ArrowDown />
+            </div>
+          </div>
+          <ul
+            id="select-box"
+            className={`absolute top-[100%] overflow-hidden ${
+              isOpenSelectBox
+                ? "block rounded-bl-[4px] rounded-br-[4px]"
+                : "hidden"
+            }`}
+          >
+            {mode === Mode.Hsl ? (
+              <li className="select-rect" onClick={() => clickOption(Mode.Hex)}>
+                {Mode.Hex}
+              </li>
+            ) : (
+              <li className="select-rect" onClick={() => clickOption(Mode.Hsl)}>
+                {Mode.Hsl}
+              </li>
+            )}
+          </ul>
+          <div className="border-gray-bg color-code hover:border-black border-[1px]">
             <span className="color-code-name">{pickedColor}</span>
             <button className="color-code-copy">
               <CopyIcon />
